1055e7157f8f8eac726cda48b36f4160d553b57b
Emanuel <emanurlperez85@gmail.com>
2025-03-05 12:35:22 -0600

    Agregar archivos de proyecto.

---
+++ ApiBiblioteca/Data/AplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBiblioteca.Data
+{
+    //Permite la interciconexion entre el String y la aplicacion para conectarse a BD
+    public class AplicationDbContext : DbContext
+    {
+        //Constructor vacio me permite inicializar cualquier clase.
+        //En nuestro caso el constructor inicializa los datos de interconexion
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Necesitamos que van hacer los set y gets.
+        //La definicion de entidades y modelos
+        //public DbSet<Productos> Productos { get; set; }
+
+        //Y aca agregamos todas las que necesitemos
+        //Usuarios, roles, categorias, facturas
+    }
+}
